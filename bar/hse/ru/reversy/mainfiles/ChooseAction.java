98f2a02d64dc6ac314ada82348dc5abe8d55f810
MasterOfFireZuko <72279831+MasterOfFireZuko@users.noreply.github.com>
2022-12-04 23:09:03 +0300

    Create ChooseAction.java

---
+++ bar/hse/ru/reversy/mainfiles/ChooseAction.java
@@ -0,0 +1,13 @@
+package bar.hse.ru.reversy.mainfiles;
+
+public interface ChooseAction {
+    void runPvM();
+    
+    void runPvwM();
+    
+    void runPvP();
+    
+    void showScore();
+    
+    void showMaxScore();
+}
