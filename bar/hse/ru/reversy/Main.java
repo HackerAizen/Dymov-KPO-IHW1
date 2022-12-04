f54c01cb42f53f853cfecb1ec0f5bf71d6e9ca35
MasterOfFireZuko <72279831+MasterOfFireZuko@users.noreply.github.com>
2022-12-04 23:03:57 +0300

    Create Main.java

---
+++ bar/hse/ru/reversy/Main.java
@@ -0,0 +1,9 @@
+package bar.hse.ru.reversy;
+import bar.hse.ru.reversy.mainfiles.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu program = Menu.getOurMenu();
+        program.startMenu();
+    }
+}
