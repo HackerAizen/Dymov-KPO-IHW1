d3c5a1b2916b506b41222ade7b56c04ff9bda073
MasterOfFireZuko <72279831+MasterOfFireZuko@users.noreply.github.com>
2022-12-04 23:07:03 +0300

    Create Gamer.java

---
+++ bar/hse/ru/reversy/description/Gamer.java
@@ -0,0 +1,9 @@
+package bar.hse.ru.reversy.description;
+
+public interface Gamer {
+    void setScore(int num);
+
+    String getName(); // никогда не думал, что в реплите от порядка может не работать
+
+    int getScore();
+}
